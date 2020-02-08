a62571eb4e31843f825bbfcf373b90bc984f5b05
Gulfz <flog-fy@hotmail.com>
2020-02-08 21:31:35 +0700

    add google map with geolocation

--- hi_flutter/lib/pages/homePage.dart
+++ hi_flutter/lib/pages/homePage.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,13 +10,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Completer<GoogleMapController> _controller = Completer();
+  Position _position;
+  Widget _child;
+
+  @override
+  void initState() {
+    _child = Center(
+      child: CircularProgressIndicator(),
+    );
+    getCurrentLocation();
+    super.initState();
+  }
+
+  void getCurrentLocation() async {
+    Position position = await Geolocator()
+        .getLastKnownPosition(desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      _position = position;
+      _child = mapWidget();
+    });
+  }
+
+  Widget mapWidget() {
+    return GoogleMap(
+      mapType: MapType.normal,
+      myLocationEnabled: true,
+      initialCameraPosition: CameraPosition(
+        target: LatLng(_position.latitude, _position.longitude),
+        zoom: 16,
+      ),
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Hi'),
       ),
-      body: Container(),
+      body: _child,
     );
   }
 }
